164725958a977b1995dc7eb017b9cb50e351665d
notzyron <notzyron@gmail.com>
2026-09-25 17:18:30 +0200

    Fix specular texture swizzling crash caused by duplicate texture paths
    - Change texturePaths in SwizzleSpecularTexture methods to a HashSet to prevent duplicate paths when materials share the same specular textures

--- UFMT/MaterialTextureAssignment/TextureSwizzler.cs
+++ UFMT/MaterialTextureAssignment/TextureSwizzler.cs
@@ -14,42 +14,51 @@ namespace UFMT.MaterialTextureAssignment
 {
     internal static class TextureSwizzler
     {
-        internal static void SwizzleSpecularTextures(string texturesPath, string[] textures)
+        internal static bool SwizzleSpecularTextures(string texturesPath, string[] textures)
         {
-            string swizzledTexturesPath = Path.Combine(texturesPath, "Swizzled");
-            if (!Directory.Exists(swizzledTexturesPath))
+            try
             {
-                var swizzledFolder = Directory.CreateDirectory(swizzledTexturesPath);
-                swizzledFolder.Attributes |= FileAttributes.Hidden;
-            }
-
-            string[] texturePaths = textures.Select(tex => Path.Combine(texturesPath, $"{tex}.png")).Where
-            (texPath => File.Exists(texPath) && !File.Exists(Path.Combine(swizzledTexturesPath, Path.GetFileName(texPath)))).ToArray();
-            Parallel.ForEach(texturePaths, texPath =>
-            {
-                Console.WriteLine($"Swizzling {Path.GetFileName(texPath)}...");
-                using Bitmap bmp = new Bitmap(texPath);
-                var rect = new Rectangle(0, 0, bmp.Width, bmp.Height);
-                var bmpData = bmp.LockBits(rect, System.Drawing.Imaging.ImageLockMode.ReadWrite, bmp.PixelFormat);
-
-                unsafe
+                string swizzledTexturesPath = Path.Combine(texturesPath, "Swizzled");
+                if (!Directory.Exists(swizzledTexturesPath))
                 {
-                    byte* ptr = (byte*)bmpData.Scan0;
-                    int bytesPerPixel = System.Drawing.Image.GetPixelFormatSize(bmp.PixelFormat) / 8;
-                    int totalBytes = bmpData.Stride * bmp.Height;
-
-                    for (int i = 0; i < totalBytes; i += bytesPerPixel)
-                    {
-                        byte blue = ptr[i];
-                        ptr[i] = ptr[i + 1];
-                        ptr[i + 1] = blue;
-                    }
+                    var swizzledFolder = Directory.CreateDirectory(swizzledTexturesPath);
+                    swizzledFolder.Attributes |= FileAttributes.Hidden;
                 }
 
-                bmp.UnlockBits(bmpData);
-                bmp.Save(Path.Combine(texturesPath, "Swizzled", Path.GetFileName(texPath)));
-                Log.Success($"Successfully Swizzled {Path.GetFileName(texPath)}!");
-            });
+                HashSet<string> texturePaths = textures.Select(tex => Path.Combine(texturesPath, $"{tex}.png")).Where
+                (texPath => File.Exists(texPath) && !File.Exists(Path.Combine(swizzledTexturesPath, Path.GetFileName(texPath)))).ToHashSet();
+                Parallel.ForEach(texturePaths, texPath =>
+                {
+                    Console.WriteLine($"Swizzling {Path.GetFileName(texPath)}...");
+                    using Bitmap bmp = new Bitmap(texPath);
+                    var rect = new Rectangle(0, 0, bmp.Width, bmp.Height);
+                    var bmpData = bmp.LockBits(rect, System.Drawing.Imaging.ImageLockMode.ReadWrite, bmp.PixelFormat);
+
+                    unsafe
+                    {
+                        byte* ptr = (byte*)bmpData.Scan0;
+                        int bytesPerPixel = System.Drawing.Image.GetPixelFormatSize(bmp.PixelFormat) / 8;
+                        int totalBytes = bmpData.Stride * bmp.Height;
+
+                        for (int i = 0; i < totalBytes; i += bytesPerPixel)
+                        {
+                            byte blue = ptr[i];
+                            ptr[i] = ptr[i + 1];
+                            ptr[i + 1] = blue;
+                        }
+                    }
+
+                    bmp.UnlockBits(bmpData);
+                    bmp.Save(Path.Combine(texturesPath, "Swizzled", Path.GetFileName(texPath)));
+                    Log.Success($"Successfully Swizzled {Path.GetFileName(texPath)}!");
+                });
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Log.Error($"An error occured while trying to swizzle specular textures: {ex.Message}");
+                return false;
+            }
         }
     }
 }
